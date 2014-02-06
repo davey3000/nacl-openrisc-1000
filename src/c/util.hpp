88a4132a32784899cab74bd8c3efd83e8a220a69
David Hart <davey3000plus@googlemail.com>
2014-02-06 18:31:10 +0000

    - Inlined endian-changing functions to improve performance.

--- src/c/util.hpp
+++ src/c/util.hpp
@@ -11,10 +11,28 @@
 
 namespace util {
 
+  /*
+   * Printf to a newly allocated C string.
+   * @param[in] format A printf format string.
+   * @param[in] args The printf arguments.
+   * @return The newly constructed string. Caller takes ownership.
+   */
   char* VprintfToNewString(const char* format, va_list args);
 
-  uint16_t ByteSwap16(uint16_t num);
-  uint32_t ByteSwap32(uint32_t num);
+  /*
+   * Swap bytes in the passed value to switch between big and little endian
+   * (or vice versa)
+   */
+  inline uint16_t ByteSwap16(uint16_t num) {
+    return ((num & 0xff) << 8) | ((num >> 8) & 0xff);
+  }
+
+  inline uint32_t ByteSwap32(uint32_t num) {
+    return (((num >> 24) & 0x000000ff) |
+            ((num <<  8) & 0x00ff0000) |
+            ((num >>  8) & 0x0000ff00) |
+            ((num << 24) & 0xff000000));
+  }
 
 }
 
